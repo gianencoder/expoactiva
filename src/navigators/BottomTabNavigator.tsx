7c68ce7fdf567622d02e9990a671a01f4c9b193f
unknown <lazaro.borghi1@gmail.com>
2023-11-15 20:34:47 -0300

    arreglo navegacion

--- src/navigators/BottomTabNavigator.tsx
+++ src/navigators/BottomTabNavigator.tsx
@@ -5,18 +5,17 @@ import { TicketsScreen } from '../screens/TicketsScreen';
 import { IconHomeComponent } from '../components/IconHomeComponent';
 import { IconUserComponent } from '../components/IconUserComponent';
 import { IconMyTicketsComponent } from '../components/IconMyTicketsComponent';
-import { View } from 'react-native'
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import { HeaderComponent } from '../components/HeaderComponent';
 import { ThemeContext } from '../context/themeContext/ThemeContext';
-import { ConfigurationScreen } from '../screens/ConfigurationScreen';
-
+import { ConfigurationNavigator } from './ConfigurationNavigator';
 
 const Tab = createBottomTabNavigator();
 
 export const BottomTabNavigator = () => {
     const { theme } = useContext(ThemeContext)
     return (
-        <View style={{ flex: 1 }}>
+        <GestureHandlerRootView style={{ flex: 1 }}>
             <HeaderComponent />
             <Tab.Navigator
                 screenOptions={({ route }) => ({
@@ -31,10 +30,10 @@ export const BottomTabNavigator = () => {
             >
                 <Tab.Screen name='Inicio' component={MyStack} options={{ tabBarIcon: ({ focused }) => (<IconHomeComponent iconSize={focused ? 25 : 22} txtSize={focused ? 15 : 12} color={focused ? theme.customColors.activeColor : theme.customColors.bottomTabIcon} />) }} />
                 <Tab.Screen name='Mis entradas' component={TicketsScreen} options={{ tabBarIcon: ({ focused }) => (<IconMyTicketsComponent iconSize={focused ? 25 : 22} txtSize={focused ? 15 : 12} color={focused ? theme.customColors.activeColor : theme.customColors.bottomTabIcon} />) }} />
-                <Tab.Screen name='Configuracion' component={ConfigurationScreen} options={{ tabBarIcon: ({ focused }) => (<IconUserComponent iconSize={focused ? 25 : 22} txtSize={focused ? 15 : 12} color={focused ? theme.customColors.activeColor : theme.customColors.bottomTabIcon} />) }} />
+                <Tab.Screen name='Configuracion' component={ConfigurationNavigator} options={{ tabBarIcon: ({ focused }) => (<IconUserComponent iconSize={focused ? 25 : 22} txtSize={focused ? 15 : 12} color={focused ? theme.customColors.activeColor : theme.customColors.bottomTabIcon} />) }} />
 
             </Tab.Navigator >
-        </View>
+        </GestureHandlerRootView>
 
     );
 }
