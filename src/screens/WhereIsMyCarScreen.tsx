e02cae322b6a52bb6ab5c74fac530351af434722
unknown <lazaro.borghi1@gmail.com>
2023-11-16 00:04:45 -0300

    ubicar mi vehiculo v1

--- src/screens/WhereIsMyCarScreen.tsx
+++ src/screens/WhereIsMyCarScreen.tsx
@@ -1,30 +1,12 @@
-import React from 'react'
-import MapView, { Marker } from 'react-native-maps';
-import { WhereIsMyCarFunction } from '../functions/WhereIsMyCarFunction';
-import { ActivityIndicator, Text, View } from 'react-native';
-import { MyColors } from '../theme/ColorsTheme';
-
+import { WhereIsMyCarMap } from '../components/WhereIsMyCarMap';
+import { View } from 'react-native';
 
 export const WhereIsMyCarScreen = () => {
-    const { isLoading, location } = WhereIsMyCarFunction()
+
+
     return (
-        isLoading ?
-            <View style={{ flex: 1, backgroundColor: 'white', justifyContent: 'center', alignItems: 'center' }}>
-                <ActivityIndicator size={'large'} color={MyColors.primary} />
-                <Text style={{ padding: 10, fontSize: 16 }}>Obteniendo información...</Text>
-            </View> :
-            <MapView
-                initialRegion={{
-                    latitude: location!.coords.latitude,
-                    longitude: location!.coords.longitude,
-                    latitudeDelta: 0.005,
-                    longitudeDelta: 0.005
-                }}
-                style={{ flex: 1 }}
-            >
-                <Marker
-                    coordinate={{ latitude: location!.coords.latitude, longitude: location!.coords.longitude }}
-                />
-            </MapView>
+        <View style={{flex:1}}>
+            <WhereIsMyCarMap />
+        </View>
     )
 }
