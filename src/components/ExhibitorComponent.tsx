58ea62ca607a8da7212725e6991d5e38da5da344
gianencoder <mendoza.gian@hotmail.com>
2023-11-22 20:39:12 -0300

    Image loader en expositores

--- src/components/ExhibitorComponent.tsx
+++ src/components/ExhibitorComponent.tsx
@@ -1,5 +1,5 @@
 import React, { useContext, useState } from 'react'
-import { Image, Text, Linking, TouchableOpacity } from 'react-native'
+import { Image, Text, Linking, TouchableOpacity, ActivityIndicator } from 'react-native'
 import { View } from 'react-native'
 import { ThemeContext } from '../context/themeContext/ThemeContext'
 import { eventStyle } from '../theme/EventTheme'
@@ -16,6 +16,7 @@ interface Props {
 
 export const ExhibitorComponent = ({ ex, selectEx }: Props) => {
   const { theme } = useContext(ThemeContext)
+  const [imgLoader, setImgLoader] = useState(false)
 
   const { goSite, callPhone, whatsapp } = ExhibitorFunction()
 
@@ -26,8 +27,9 @@ export const ExhibitorComponent = ({ ex, selectEx }: Props) => {
         onPress={() => selectEx(ex.id)}
       >
         <View style={eventStyle.event}>
-          <View style={eventStyle.eventListImg}>
-            {ex.logo === null || !ex.logo ? <Image style={{ ...eventStyle.img, height: 120, width: 105 }} source={require('../assets/images/noPhoto.jpg')} /> : <Image style={eventStyle.img} source={{ uri: ex.logo.toString() }} />}
+          <View style={{ ...eventStyle.eventListImg, justifyContent: 'center', alignItems: 'center' }}>
+            {imgLoader && <ActivityIndicator style={{ justifyContent: 'center', position: 'absolute', alignItems: 'center' }} color={theme.customColors.activeColor} />}
+            {ex.logo === null || !ex.logo ? <Image style={{ ...eventStyle.img, height: 120, width: 105 }} source={require('../assets/images/noPhoto.jpg')} /> : <Image onLoadStart={() => setImgLoader(true)} onLoadEnd={() => setImgLoader(false)} style={{ height: 100, width: 100, borderRadius: 15 }} source={{ uri: ex.logo.toString() }} />}
           </View>
           <View style={{ ...eventStyle.eventListTitle, marginVertical: 5 }}>
             <Text numberOfLines={1} style={{ ...eventStyle.titleTxt, color: theme.colors.text, textTransform: 'uppercase' }}>{ex.name}</Text>
