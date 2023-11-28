581af0a545ed2a0e6f1d890ca1cf7abd156de88d
SebastianMerenyi <sebastian.merenyi@estudiantes.utec.edu.uy>
2023-11-27 21:26:48 -0300

    ajuste de ortografía

--- src/util/utils.tsx
+++ src/util/utils.tsx
@@ -572,7 +572,7 @@ export const translations = {
             placeholder: "Your code here...",
             redeemButtonText: {
                 withCode: "REDEEM",
-                withoutCode: "ENTER A CODE"
+                withoutCode: "ENTER CODE"
             },
             cancelButtonText: "Cancel"
         },
@@ -641,7 +641,7 @@ export const translations = {
         },
         visibilityScree: {
             activarModoOscuro: 'Activate Dark Mode',
-            desactivarModoOscuro: 'Deactivate Dark Mode',
+            desactivarModoOscuro: 'Desactivate Dark Mode',
         },
         bottomTab: {
             inicio: "Home",
